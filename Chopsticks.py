f3a3e9a5b1935df35692ccc31b545a4f23a80362
Mike Kusold <Kusold@gmail.com>
2012-11-19 13:10:44 -0500

    Follows the Python Style Guide more closely

--- Chopsticks.py
+++ Chopsticks.py
@@ -5,52 +5,52 @@ class Namespace: pass
 def main(args):
     ## Variables ##
     ns = Namespace()
-    ns.numOfTestCases = 0
-    ns.numOfGuests = 0             # Including family
-    ns.numOfChopsticks = 0
-    ns.availableChopsticks = []    # Holds all chopsticks that are still sitting in drawer
-    ns.originalChopsticks= []     # ALL Chopsticks. Never modified until new test case
-    ns.inputList = []
+    ns.num_of_test_cases = 0
+    ns.num_of_guests = 0             # Including family
+    ns.num_of_chopsticks = 0
+    ns.available_chopsticks = []    # Holds all chopsticks that are still sitting in drawer
+    ns.original_chopsticks= []     # ALL Chopsticks. Never modified until new test case
+    ns.input_list = []
 
-    def populateListFromInput(file):
+    def populate_list_from_input(file):
         try:
             input = open(file, 'r').read().splitlines()
         except:
             print "Unable to open file"
             exit(3)
-        ns.inputList = splitInputFile(input)
+        ns.input_list = split_input_file(input)
 
         # Error Handling *should* be added here, but I am trusting the user for valid inputs.
-        ns.numOfTestCases = int(ns.inputList.pop(0).pop(0))
+        ns.num_of_test_cases = int(ns.input_list.pop(0).pop(0))
 
-    def getTestCaseData():
-        line = ns.inputList.pop(0)
-        ns.numOfGuests = int(line.pop(0))
-        ns.numOfGuests += 8 # Add 8 to account for Mr. L's immediate family
-        ns.numOfChopsticks = int(line.pop(0))
-        ns.availableChopsticks = ns.inputList.pop(0)
-        ns.availableChopsticks = map(int, ns.availableChopsticks) # Convert from str to int
-        ns.originalChopsticks = ns.availableChopsticks
+    def get_test_case_data():
+        line = ns.input_list.pop(0)
+        ns.num_of_guests = int(line.pop(0))
+        ns.num_of_guests += 8 # Add 8 to account for Mr. L's immediate family
+        ns.num_of_chopsticks = int(line.pop(0))
+        ns.available_chopsticks = ns.input_list.pop(0)
+        ns.available_chopsticks = map(int, ns.available_chopsticks) # Convert from str to int
+        ns.original_chopsticks = ns.available_chopsticks
 
-    def splitInputFile(input):
+    def split_input_file(input):
         list = []
         for item in input:
             temp = item.split(' ')
             list.append(temp)
         return list
 
-    def groupChopsticks():
-        potentialSets = []
+    def group_chopsticks():
+        potential_sets = []
         pos = 0
-        while pos < len(ns.availableChopsticks) - 1:
-            tempGroup = [ns.availableChopsticks[pos],ns.availableChopsticks[pos + 1]]
-            badness = (ns.availableChopsticks[pos] - ns.availableChopsticks[pos + 1]) * (ns.availableChopsticks[pos] - ns.availableChopsticks[pos + 1])
-            potentialSets.append([tempGroup, badness])
+        while pos < len(ns.available_chopsticks) - 1:
+            temp_group = [ns.available_chopsticks[pos],ns.available_chopsticks[pos + 1]]
+            badness = (ns.available_chopsticks[pos] - ns.available_chopsticks[pos + 1]) * (ns.available_chopsticks[pos] - ns.available_chopsticks[pos + 1])
+            potential_sets.append([temp_group, badness])
             pos += 1
-        return potentialSets
+        return potential_sets
 
-    def getLongerChopstick(size):
-        reversed = ns.availableChopsticks
+    def get_longer_chopstick(size):
+        reversed = ns.available_chopsticks
         reversed.reverse()
 
         if reversed[0] > size[0] and reversed > size[1]:
@@ -58,15 +58,15 @@ def main(args):
         else:
             return None
 
-    def getBarelyLongerChopstick(size):
-        set = ns.availableChopsticks
+    def get_barely_longer_chopstick(size):
+        set = ns.available_chopsticks
         
         for item in set:
             if item > size[0] and item > size[1]:
                 return item
         return None
 
-    def getLargestBadness(list):
+    def get_largest_badness(list):
         largest = 0
 
         for item in list:
@@ -74,56 +74,56 @@ def main(args):
                 largest = item[1]
         return largest
 
-    def totalBadness(list):
+    def total_badness(list):
         total = 0
         for item in list:
             total += item[1]
         return total
 
     # Read in input and assign values
-    inputFileLocation = args[1]
-    #inputFileLocation = "C:/Sample.txt"
-    populateListFromInput(inputFileLocation)
+    input_file_location = args[1]
+    #input_file_location = "C:/Sample.txt"
+    populate_list_from_input(input_file_location)
 
     # Run X - Test cases
-    for testcase in range(ns.numOfTestCases):
-        getTestCaseData()
-        tableSetting = []
-        for guest in range(ns.numOfGuests):
+    for testcase in range(ns.num_of_test_cases):
+        get_test_case_data()
+        table_setting = []
+        for guest in range(ns.num_of_guests):
             # Find Neighboring Pairs with lowest badness score
-            chopstickOptions = groupChopsticks()
+            chopstick_options = group_chopsticks()
 
-            sortedChopstickOptions = sorted(chopstickOptions, key=lambda x:(x[1], -x[0][0]))
+            sorted_chopstick_options = sorted(chopstick_options, key=lambda x:(x[1], -x[0][0]))
 
-            chopstickToPlaceOnTable = []
-            bool = True
-            while(bool):
+            chopstick_to_place_on_table = []
+
+            while(True):
                 #Remove the longestChopsticks with the lowest Badness score
-                chopsticks = sortedChopstickOptions.pop(0)
+                chopsticks = sorted_chopstick_options.pop(0)
 
                 #Check that there are chopsticks available that are larger
-                #longChopstick = getLongerChopstick(chopsticks[0])
-                longChopstick = getBarelyLongerChopstick(chopsticks[0])
-                if longChopstick != None:
-                    chopsticks[0].append(longChopstick)
+                #long_chopstick = get_longer_chopstick(chopsticks[0])
+                long_chopstick = get_barely_longer_chopstick(chopsticks[0])
+                if long_chopstick != None:
+                    chopsticks[0].append(long_chopstick)
                     # Remove all used chopsticks
-                    ns.availableChopsticks.remove(chopsticks[0][0])
-                    ns.availableChopsticks.remove(chopsticks[0][1])
-                    ns.availableChopsticks.remove(chopsticks[0][2])
-                    bool = False
-                    chopstickToPlaceOnTable = chopsticks
+                    ns.available_chopsticks.remove(chopsticks[0][0])
+                    ns.available_chopsticks.remove(chopsticks[0][1])
+                    ns.available_chopsticks.remove(chopsticks[0][2])
+                    chopstick_to_place_on_table = chopsticks
+                    break
                 else:
-                    if len(sortedChopstickOptions) == 0:
+                    if len(sorted_chopstick_options) == 0:
                         print "Unable to find longer chopstick"
                         exit(3)
             
-            tableSetting.append(chopstickToPlaceOnTable)
+            table_setting.append(chopstick_to_place_on_table)
 
         # Compute largest badness
         print "\n\nTest Case: " + str(testcase)
-        print "Total Badness: " + str(totalBadness(tableSetting))
+        print "Total Badness: " + str(total_badness(table_setting))
         print "\nChopstick Pairs: "
-        print tableSetting
+        print table_setting
     raw_input()
 
 main(sys.argv)
